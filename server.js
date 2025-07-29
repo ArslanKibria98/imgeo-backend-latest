94ae10baaeb3be167ad29ddd778a502c3799023a
Arslan kibria <arslankibria98@gmail.com>
2025-07-30 02:04:16 +0500

    latestt for deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const User = require("../imgeo-backend-new/models/user");
+const User = require("./models/user");
 const app = express();
 
 // Body Parsers
